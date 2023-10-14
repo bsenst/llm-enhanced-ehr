d3dec8d7cb7e664ab1667fe938d98d89a1f2729f
bsenst <benjamin.senst@gmail.com>
2023-10-14 12:09:29 +0200

    update app for changed secret management

--- streamlit/app.py
+++ streamlit/app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 
 from langchain.llms import Clarifai
+from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import AnalyzeDocumentChain
 
@@ -22,14 +23,11 @@ with open("streamlit/assets/medical_note1.txt") as infile:
 #     pat=os.environ.get("CLARIFAI_PAT"), user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID
 # )
 
-from langchain.llms import OpenAI
-llm = OpenAI(temperature=0, openai_api_key=os.environ.get("OPENAI"))
-
+llm = OpenAI(temperature=0, openai_api_key=os.environ.get("openai"))
 qa_chain = load_qa_chain(llm)
 qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=qa_chain)
 
 medical_note = st.session_state["medical_note1"]
-
 question = st.text_input("Enter your query for the medical note")
 
 if question:
