5d75155561c91a4770676e0b664bbc29bfd8847a
bsenst <benjamin.senst@gmail.com>
2023-10-14 12:35:21 +0200

    update medical note 1 examples

--- streamlit/pages/medical_note_1.py
+++ streamlit/pages/medical_note_1.py
@@ -4,7 +4,7 @@ st.title("Medical Note 1")
 
 st.write(st.session_state["medical_note1"])
 
-st.header("Example LLM Questions & Answers")
+st.title("Example LLM Questions & Answers")
 
 with st.expander("What conditions does the patient have?"):
     st.write("Hypertension, Hyperlipidemia, Type 2 Diabetes Mellitus.")
@@ -14,3 +14,7 @@ with st.expander("What medications does the patient take?"):
 
 with st.expander("List the medical conditions and medications for each!"):
     st.write("Medical Conditions: Hypertension, Hyperlipidemia, Type 2 Diabetes Mellitus. Medications: Lisinopril 20 mg daily, Atorvastatin 40 mg daily, Metformin 1000 mg twice daily, Naproxen 500 mg twice daily as needed.")
+    st.caption("Interesting: the previous disc herniation is not listed")
+
+with st.expander("What is the recent complaint of the patient?"):
+    st.write("The patient's recent complaint is lower back pain on the right side of his lower back, described as a dull ache with an intensity of 6/10 on the pain scale.")
